060078a31679601f0b3876f1874336e58cdef1f5
oshman <kraffo61@gmail.com>
2021-03-26 21:21:05 +0300

    initial commit, added a main file with basic ARS-net class

---
+++ neural-nets/task2/main.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+#include <cmath>
+
+
+/****************** class ART - neural net *********************************/
+class ArtNet
+{
+public:
+    std::vector <int> inputVals_T;
+    std::vector <int> outputVals_B;
+    void  initialize(const std::vector<int> inputVals);
+
+
+private:
+
+};
+
+void ArtNet::initialize(const std::vector<int> inputVals)
+{
+
+}
+
+
+int main()
+{
+    return 0;
+}
